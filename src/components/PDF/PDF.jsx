dec604f2218f283e43ddc0ef60fd87c1008f72d9
FreelancerTasin507 <freelancertasin507@gmail.com>
2023-07-02 13:41:02 +0600

    added

---
+++ src/components/PDF/PDF.jsx
@@ -0,0 +1,13 @@
+const PDF = () => {
+  return (
+    <div>
+      <a href="../../../public/front end resume2.pdf" download>
+        <button className="btn btn-outline duration-300 ml-4">
+          Download Resume
+        </button>
+      </a>
+    </div>
+  );
+};
+
+export default PDF;
