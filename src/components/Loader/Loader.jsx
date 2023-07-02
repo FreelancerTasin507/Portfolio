9a0f7cb63ba941caa40ef105daac5f12fc4710b4
FreelancerTasin507 <freelancertasin507@gmail.com>
2023-07-02 17:57:07 +0600

    added

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import { ThreeDots } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <div className=" mt-[20%] ml-[45%]">
+    <div className=" lg:mt-[20%] md:mt-[30%] mt-[60%] ml-[45%]">
       <ThreeDots
         height="80"
         width="80"
--- src/components/PDF/PDF.jsx
+++ src/components/PDF/PDF.jsx
@@ -1,14 +1,31 @@
+// const Pdf = () => {
+//     return (
+//       <div>
+//         <a href="../../../public//front end resume2.pdf" download>
+//           <button className="btn btn-outline duration-300 ml-4">
+//             Download Resume
+//           </button>
+//         </a>
+//       </div>
+//     );
+//   };
+  
+//   export default Pdf;
+  
+
 const Pdf = () => {
-    return (
-      <div>
-        <a href="../../../public//front end resume2.pdf" download>
-          <button className="btn btn-outline duration-300 ml-4">
-            Download Resume
-          </button>
-        </a>
-      </div>
-    );
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '../../../public/front end resume2.pdf'; // Path to your resume PDF
+    link.download = 'resume.pdf'; // Optional: Set the downloaded file name
+    link.click();
   };
-  
-  export default Pdf;
-  
+
+  return (
+    <button className="btn btn-outline duration-300 ml-4" onClick={handleDownload}>
+      Download Resume
+    </button>
+  );
+};
+
+export default Pdf;
